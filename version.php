cc5296515c76bdb5b56110bc02ccb69b696d343d
Sebastian Gomez Velasco <sebastian.gomez.velasco@gmail.com>
2016-08-25 12:26:54 -0700

    First file

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
+
+$plugin->version = 2016082512;
+$plugin->component = 'local_as_sync';
+$plugin->requires = 2013111800;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->cron = 60;
