946ee1a2a00dfe8605d4976dd020c5b71fa98d1f
Sebastian Gomez Velasco <sebastian.gomez.velasco@gmail.com>
2016-08-25 16:26:44 -0700

    Add comment to version.php

--- version.php
+++ version.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Version information
+ *
+ * @package    local
+ * @subpackage as_sync
+ * @author     Sebastian Gomez Velasco (sebastian.gomez.velasco@gmail.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
